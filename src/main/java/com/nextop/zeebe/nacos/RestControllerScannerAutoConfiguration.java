43a22b44d533aca985aee597da522ebab7dad441
jeffrey <13910661166@139.com>
2021-09-25 14:50:39 +0800

    Initial commit

---
+++ src/main/java/com/nextop/zeebe/nacos/RestControllerScannerAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.nextop.zeebe.nacos;
+
+import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.discovery.NacosDiscoveryAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jeffrey
+ */
+
+@Configuration
+@AutoConfigureBefore(NacosDiscoveryAutoConfiguration.class)
+public class RestControllerScannerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(value = {"nextop.zeebe.nacos.metadata.setting"}, matchIfMissing = true)
+    public NacosDiscoveryProperties discoveryProperties(ApplicationContext applicationcontext) {
+        NacosDiscoveryProperties properties = new NacosDiscoveryProperties();
+        RestControllerScanner scanner = new RestControllerScanner(applicationcontext, properties);
+        scanner.scan();
+        properties.getMetadata().put("nextop.zeebe.nacos.metadata.enabled", "true");
+        return properties;
+    }
+}
